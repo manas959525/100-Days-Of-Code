ed9b2707a4570ae4e3add3126127e12d28da0fc0
MANAS SINGH SHEKHAWAT <manasshekhawat095@gmail.com>
2026-09-19 22:46:52 +0530

    Add files via upload
    Day41

---
+++ ques81.c
@@ -0,0 +1,14 @@
+//Q81: Count characters in a string without using built-in length functions.
+
+#include<stdio.h>
+int main() {
+    char str[100];
+    int count = 0;
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    while (str[count] != '\0') {
+        count++;
+    }
+    printf("The number of characters in the string is: %d\n", count - 1); // Subtracting 1 to exclude the newline character added by fgets
+    return 0;
+}
